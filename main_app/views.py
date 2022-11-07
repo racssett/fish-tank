307a77224a31da31045034498f47cac79daabc5a
racssett <rachel.c.hassett@gmail.com>
2022-11-07 00:00:33 -0600

    update views to accept data from interactive shell

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,15 @@
 from django.shortcuts import render
+from .models import Fish
 
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Hello</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def fish_index(request):
+  fish = Fish.objects.all()
   return render(request, 'fish/index.html', { 'fish': fish })
 
-
-class Fish: 
-  def __init__(self, name, type, description, age):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.age = age
-
-fish = [
-  Fish('Lolo', 'clownfish', 'Kinda funny.', 3),
-  Fish('Sachi', 'angel fish', 'Looks like an angel.', 0),
-  Fish('Fancy', 'starfish', 'Happy orange star.', 4),
-  Fish('Bonk', 'unknown', 'Blubs loudly.', 6)
-]
