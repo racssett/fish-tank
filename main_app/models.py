d0f32e91a36862d063f33b2839e3b72fcc7eff4c
racssett <rachel.c.hassett@gmail.com>
2022-11-07 20:42:15 -0600

    Add delete and update functionality for fish

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Fish(models.Model):
   name = models.CharField(max_length=100)
@@ -8,3 +9,7 @@ class Fish(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse("fish_detail", kwargs={"fish_id": self.id})
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Fish
 
 from django.http import HttpResponse
@@ -16,3 +17,16 @@ def fish_index(request):
 def fish_detail(request, fish_id):
   fish = Fish.objects.get(id=fish_id)
   return render(request, 'fish/detail.html', { 'fish': fish })
+
+class FishCreate(CreateView):
+  model = Fish
+  fields = '__all__'
+  success_url = '/fish/'
+
+class FishUpdate(UpdateView):
+  model = Fish
+  fields = '__all__'
+
+class FishDelete(DeleteView):
+  model = Fish
+  success_url = '/fish/'
